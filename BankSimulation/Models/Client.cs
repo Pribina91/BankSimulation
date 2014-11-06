37d45c5eff759699a344ee020c581687fad0ef88
pribina91 <adostajo@gmail.com>
2014-11-07 00:46:32 +0100

    before change to MVC

---
+++ BankSimulation/Models/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BankSimulation.Models
+{
+	public class Client
+	{
+		[Key]
+		public int idClient { get; set; }
+		public String name { get; set; }
+
+		public List<BankAccount> accounts;
+		
+		public Client(String _name)
+		{
+			this.name = _name;
+			accounts = new List<BankAccount>();
+		}
+
+		public void addAccount(Currency curr)
+		{
+			BankAccount newAccount = new BankAccount();
+			//newAccount
+
+			accounts.Add(newAccount);
+		}
+	}
+}
---
+++ BankSimulation/Models/Currency.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace BankSimulation.Models
+{
+	public class Currency
+	{
+		public Currency()
+		{
+
+		}
+		[Key]
+		public int idCurrency { get; set; }
+		double _rate;
+
+		public double rate
+		{
+			get { return this._rate; }
+			set { this._rate = Math.Round(value, 3); }
+		}
+		public int units { set; get; }
+
+	}
+}
